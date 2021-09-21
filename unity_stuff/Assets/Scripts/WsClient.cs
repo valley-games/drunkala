cd160fa507bce0b5ae9947f18215a75527cccfc7
mifulapirus <mifulapirus@gmail.com>
2021-09-21 18:05:43 +0300

    add unity stuff

---
+++ unity_stuff/Assets/Scripts/WsClient.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using WebSocketSharp;
+using UnityEngine.UI;
+
+public class WsClient : MonoBehaviour
+{
+    WebSocket ws;
+     public InputField mainInputField;
+
+    private void Start()
+    {
+        ws = new WebSocket("ws://localhost:4000/api/stream?name=sdf&code=TftpZf");
+        //ws = new WebSocket("http://localhost:4000/api/state");
+        
+        ws.Connect();
+        ws.OnMessage += (sender, e) => {
+            Debug.Log("Message Received from "+((WebSocket)sender).Url+", Data : "+e.Data);
+        };
+    }
+
+    private void SubmitName(string arg0) {
+         Debug.Log(arg0);
+     }
+
+    private void Update(){
+        if (Input.GetKeyUp(KeyCode.Return)) { TapYourSubmitFunction(); }
+
+        if(ws == null) {
+            return;
+        }
+ 
+        if (Input.GetKeyDown(KeyCode.Space)){
+            ws.Send("Hello");
+            Debug.Log("Yeah");
+        }  
+    }
+}
